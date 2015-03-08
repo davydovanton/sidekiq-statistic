e7766b56ccf89fa6380b525975fd7fe797ab7776
Anton Davydov <antondavydov.o@gmail.com>
2015-03-08 16:42:13 +0300

    Fix charter template

--- lib/sidekiq/history/web_extension.rb
+++ lib/sidekiq/history/web_extension.rb
@@ -9,6 +9,7 @@ module Sidekiq
 
         app.get '/history' do
           @worker_statistic = Sidekiq::History::WorkerStatistic.new(20)
+          @tooltip_template = '<%= datasetLabel %> - <%= value %>'
           render(:erb, File.read(File.join(view_path, 'history.erb')))
         end
       end
