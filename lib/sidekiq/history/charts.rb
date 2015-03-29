3581bcd7a74d1dd47db2280daff21cf5d843e824
Anton Davydov <antondavydov.o@gmail.com>
2015-03-29 18:49:37 +0300

    Set specific color for any worker

--- lib/sidekiq/history/charts.rb
+++ lib/sidekiq/history/charts.rb
@@ -8,12 +8,12 @@ module Sidekiq
 
       def information_for(type)
         redis_statistic.worker_names.map do |worker|
-          color_hash = random_color_hash
+          color = color_for(worker)
           {
             label: worker,
-            fillColor: "rgba(#{color_hash},0.2)",
-            strokeColor: "rgba(#{color_hash},0.9)",
-            pointColor: "rgba(#{color_hash},0.2)",
+            fillColor: "rgba(#{color},0.2)",
+            strokeColor: "rgba(#{color},0.9)",
+            pointColor: "rgba(#{color},0.2)",
             pointStrokeColor: '#fff',
             pointHighlightFill: '#fff',
             pointHighlightStroke: 'rgba(220,220,220,1)',
@@ -22,8 +22,11 @@ module Sidekiq
         end
       end
 
-      def random_color_hash
-        [Random.new.rand(256), Random.new.rand(256), Random.new.rand(256)].join ','
+      def color_for(worker)
+        Digest::MD5.hexdigest(worker)[0..5]
+          .scan(/../)
+          .map{ |color| color.to_i(16) }
+          .join ','
       end
 
       def dates
