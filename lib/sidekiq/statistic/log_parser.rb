313e4c7a69a711d529fb4dd865b20d8618346ea4
Anton Davydov <antondavydov.o@gmail.com>
2015-07-28 14:33:13 +0300

    Read first 1_000 lines from changelog

--- lib/sidekiq/statistic/log_parser.rb
+++ lib/sidekiq/statistic/log_parser.rb
@@ -3,6 +3,7 @@ module Sidekiq
     # Heroku have read only file system. See more in this link:
     # https://devcenter.heroku.com/articles/read-only-filesystem
     class LogParser
+      FILE_LINES_COUNT = 1_000
       def initialize(worker_name)
         @worker_name = worker_name
         @logfile = log_file
@@ -11,9 +12,11 @@ module Sidekiq
       def parse
         return [] unless File.exists?(@logfile)
 
-        File.open(@logfile).map do |line|
-          line_hash(line) if line[/\W?#@worker_name\W?/]
-        end.compact
+        File
+          .readlines(@logfile)
+          .first(FILE_LINES_COUNT)
+          .map{ |line| line_hash(line) if line[/\W?#@worker_name\W?/] }
+          .compact
       end
 
       def line_hash(line)
@@ -22,9 +25,9 @@ module Sidekiq
 
       def color(line)
         case
-        when line.include?('done') then 'green'
+        when line.include?('done')  then 'green'
         when line.include?('start') then 'yellow'
-        when line.include?('fail') then 'red'
+        when line.include?('fail')  then 'red'
         end
       end
 
