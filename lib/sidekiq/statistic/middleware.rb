fe16377674234a0b3cc6e4dd200c1911a8a08055
Mike Perham <mperham@gmail.com>
2015-08-18 15:23:58 -0700

    Middleware refactoring
    1. Simplify code, remove unnecessary methods
    2. Use a single finish time to ensure consistency of metrics
    3. Minimize object allocation, e.g. don't allocate 'statistics' array
    4. Pipeline redis commands for performance

--- lib/sidekiq/statistic/middleware.rb
+++ lib/sidekiq/statistic/middleware.rb
@@ -3,24 +3,19 @@ module Sidekiq
     class Middleware
       attr_accessor :msg
 
-      def call(worker, msg, queue, &block)
-        call_with_sidekiq_Statistic(worker, msg, queue, &block)
-      end
-
-      private
-
-      def call_with_sidekiq_Statistic(worker, msg, queue)
+      def call(worker, msg, queue)
         worker_status = { last_job_status: 'passed'.freeze }
-        start = Time.now.utc
+        start = Time.now
 
         yield
-      rescue StandardError => e
+      rescue => e
         worker_status[:last_job_status] = 'failed'.freeze
 
         raise e
       ensure
-        worker_status[:time] = elapsed(start)
-        worker_status[:last_runtime] = Time.now.utc
+        finish = Time.now
+        worker_status[:last_runtime] = finish.utc
+        worker_status[:time] = (finish - start).to_f.round(3)
         worker_status[:class] = msg['wrapped'.freeze] || worker.class.to_s
 
         save_entry_for_worker worker_status
@@ -28,29 +23,23 @@ module Sidekiq
 
       def save_entry_for_worker(worker_status)
         status = worker_status.dup
-        realtime_hash = "#{REDIS_HASH}:realtime:#{Time.now.sec}"
-        worker_key = "#{Time.now.utc.to_date}:#{status.delete :class}"
+        time = worker_status[:last_runtime]
+        realtime_hash = "#{REDIS_HASH}:realtime:#{time.sec}"
+        worker_key = "#{time.to_date}:#{status.delete :class}"
 
         Sidekiq.redis do |redis|
-          statistics = [
-            "#{worker_key}:last_job_status", status[:last_job_status],
-            "#{worker_key}:last_time", status[:last_runtime]
-          ]
+          redis.pipelined do
+            redis.hincrby REDIS_HASH, "#{worker_key}:#{status[:last_job_status]}", 1
+            redis.hmset REDIS_HASH, "#{worker_key}:last_job_status", status[:last_job_status],
+                                    "#{worker_key}:last_time", status[:last_runtime]
+            redis.lpush "#{worker_key}:timeslist", status[:time]
 
-          redis.hincrby REDIS_HASH, "#{worker_key}:#{status[:last_job_status]}", 1
-
-          redis.hmset REDIS_HASH, statistics
-          redis.lpush "#{worker_key}:timeslist", status[:time]
-
-          redis.hincrby realtime_hash, "#{status[:last_job_status]}:#{worker_status[:class]}", 1
-          redis.expire realtime_hash, 2
+            redis.hincrby realtime_hash, "#{status[:last_job_status]}:#{worker_status[:class]}", 1
+            redis.expire realtime_hash, 2
+          end
         end
       end
 
-      # this methos already exist in Sidekiq::Middleware::Server::Logging class
-      def elapsed(start)
-        (Time.now.utc - start).to_f.round(3)
-      end
     end
   end
 end
