2a58fa6305264310bfa4162d642da9a0fafc3e7c
Felix Bünemann <buenemann@louis.info>
2015-08-20 21:45:50 +0200

    Fix statistics display for nested worker classes
    This fixes issue #46, where statistics are parsed improperly if a worker
    class is nested in a module or class, eg. `Nested::HistoryWorker`.

--- lib/sidekiq/statistic/statistic/realtime.rb
+++ lib/sidekiq/statistic/statistic/realtime.rb
@@ -20,7 +20,7 @@ module Sidekiq
           conn
             .hgetall("#{REDIS_HASH}:realtime:#{Time.now.sec - 1}")
             .each do |keys, value|
-              *keys, last = keys.split(':'.freeze)
+              *keys, last = keys.split(KEY_SEPARATOR)
               keys.inject(redis_hash, &key_or_empty_hash)[last] = value.to_i
             end
 
