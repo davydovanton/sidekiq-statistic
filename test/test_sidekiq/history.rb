80869323db40f1ab2dc85154f93cb7e35216a717
Anton Davydov <antondavydov.o@gmail.com>
2015-08-23 15:25:56 +0300

    Delete unnecessary test

--- test/test_sidekiq/history.rb
+++ test/test_sidekiq/history.rb
@@ -4,8 +4,4 @@ class TestSidekiq::Statistic < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Sidekiq::Statistic::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
