4550b6c70cb2cdeb645078c898906914a0f18b9b
Wender Freese <wender.jean@gmail.com>
2019-12-06 13:44:23 -0300

    Refactoring colors helper to avoid light colors

--- lib/sidekiq/statistic/helpers/color.rb
+++ lib/sidekiq/statistic/helpers/color.rb
@@ -4,22 +4,38 @@ module Sidekiq
   module Statistic
     module Helpers
       module Color
-        def for(worker_name, format = :rgb)
-          rgb = Digest::MD5.hexdigest(worker_name)[0..5]
-                           .scan(/../)
-                           .map { |color| color.to_i(16) }
-                           .join(',')
+        class << self
+          BASE = 16
+          LIMIT_NUMBERT_TO_AVOID_WHITES = 215
 
-          return to_hex(rgb) if format == :hex
+          def for(phrase, format: :rgb)
+            return hex(phrase) if format == :hex
 
-          rgb
+            rgb(phrase)
+          end
+
+          private
+
+          def rgb(phrase)
+            digested_unique_color(phrase).join(',')
+          end
+
+          def hex(phrase)
+            '#' + digested_unique_color(phrase).map do |number|
+              number.to_s(BASE).rjust(2, '0')
+            end.join
+          end
+
+          def digested_unique_color(phrase)
+            Digest::MD5.hexdigest(phrase)[0..5]
+                       .scan(/../)
+                       .map(&method(:hex_pair_to_number))
+          end
+
+          def hex_pair_to_number(pair)
+            pair.to_i(BASE) % LIMIT_NUMBERT_TO_AVOID_WHITES
+          end
         end
-
-        def to_hex(rgb)
-          '#' + rgb.split(',').map { |v| v.to_i.to_s(16).rjust(2, '0').upcase }.join
-        end
-
-        module_function :for, :to_hex
       end
     end
   end
