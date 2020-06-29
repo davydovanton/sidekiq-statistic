3b8cb5f78da875b318060d628fd425f83fef5048
Gabriel Rubens <gabrielrumiranda@gmail.com>
2020-06-29 09:49:37 -0300

    Fix deprecation warnings related to Minitest 6 (#147)

--- test/test_sidekiq/web_extension_test.rb
+++ test/test_sidekiq/web_extension_test.rb
@@ -28,9 +28,9 @@ module Sidekiq
       end
 
       it 'can display home with statistic tab' do
-        last_response.status.must_equal 200
-        last_response.body.must_match(/Sidekiq/)
-        last_response.body.must_match(/Statistic/)
+        _(last_response.status).must_equal 200
+        _(last_response.body).must_match(/Sidekiq/)
+        _(last_response.body).must_match(/Statistic/)
       end
     end
 
@@ -40,24 +40,24 @@ module Sidekiq
       end
 
       it 'can display statistic page without any failures' do
-        last_response.status.must_equal 200
-        last_response.body.must_match(/statistic/)
+        _(last_response.status).must_equal 200
+        _(last_response.body).must_match(/statistic/)
       end
 
       describe 'when there are statistic' do
         it 'should be successful' do
-          last_response.status.must_equal 200
+          _(last_response.status).must_equal 200
         end
       end
 
       it 'can display worker table' do
-        last_response.body.must_match(/Worker/)
-        last_response.body.must_match(/Date/)
-        last_response.body.must_match(/Success/)
-        last_response.body.must_match(/Failure/)
-        last_response.body.must_match(/Total/)
-        last_response.body.must_match(/Time\(sec\)/)
-        last_response.body.must_match(/Average\(sec\)/)
+        _(last_response.body).must_match(/Worker/)
+        _(last_response.body).must_match(/Date/)
+        _(last_response.body).must_match(/Success/)
+        _(last_response.body).must_match(/Failure/)
+        _(last_response.body).must_match(/Total/)
+        _(last_response.body).must_match(/Time\(sec\)/)
+        _(last_response.body).must_match(/Average\(sec\)/)
       end
     end
 
@@ -67,7 +67,7 @@ module Sidekiq
       end
 
       it 'can display statistic page without any failures' do
-        last_response.status.must_equal 200
+        _(last_response.status).must_equal 200
         response = JSON.parse(last_response.body)
 
         assert_includes response, 'date'
@@ -79,7 +79,7 @@ module Sidekiq
 
       describe 'when there are statistic' do
         it 'should be successful' do
-          last_response.status.must_equal 200
+          _(last_response.status).must_equal 200
         end
       end
     end
