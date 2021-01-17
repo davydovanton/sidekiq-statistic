27be4832fd0fba45221351bd60ff19fcf338fb78
Wender Freese <wender.jean@gmail.com>
2021-01-17 15:41:24 -0300

    Convert indentation tabs to spaces in "store_test.rb"

--- test/statistic/metrics/store_test.rb
+++ test/statistic/metrics/store_test.rb
@@ -3,8 +3,8 @@
 require 'minitest_helper'
 
 describe Sidekiq::Statistic::Metrics::Store do
-	# The behavior from this class is fully exercised on the
-	# integration test below:
-	#
-	# test/test_sidekiq/middleware_test.rb
+  # The behavior from this class is fully exercised on the
+  # integration test below:
+  #
+  # test/test_sidekiq/middleware_test.rb
 end
