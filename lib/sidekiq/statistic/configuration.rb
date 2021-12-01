1cb8aac3d7552a92be90f5800e7dad829268cf7d
carlosdanielpohlod <carlospohlod@gmail.com>
2021-12-01 10:02:41 -0300

    added configuration :max_timelist_length validation and test

--- lib/sidekiq/statistic/configuration.rb
+++ lib/sidekiq/statistic/configuration.rb
@@ -8,6 +8,15 @@ module Sidekiq
       def initialize
         @max_timelist_length = 250_000
       end
+
+      def max_timelist_length=(value)
+        if(value.is_a?(Numeric) && value > 0)
+          @max_timelist_length = value
+        else
+          raise ArgumentError, 'max_timelist_length must be a positive number'
+        end
+      end
+
     end
   end
 end
--- test/test_sidekiq/configuration_test.rb
+++ test/test_sidekiq/configuration_test.rb
@@ -11,6 +11,16 @@ module Sidekiq
           config.max_timelist_length = 12345
           assert_equal 12345, config.max_timelist_length
         end
+
+        it 'raises error if value is not a number' do
+          config = Configuration.new
+          assert_raises(ArgumentError) { config.max_timelist_length = nil }
+        end
+
+        it 'raises error if value is less than 1' do
+          config = Configuration.new
+          assert_raises(ArgumentError) { config.max_timelist_length = 0 }
+        end
       end
     end
   end
